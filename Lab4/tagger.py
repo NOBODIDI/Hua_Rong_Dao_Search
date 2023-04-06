e77a6d595327d63801c5e749b5ac32a95460c9b0
Adrien Mery <86313865+NOBODIDI@users.noreply.github.com>
2023-04-06 03:03:20 +0000

    Importing the training file

---
+++ Lab4/tagger.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import argparse
+import numpy as np
+
+TAGS = ["AJ0", "AJC", "AJS", "AT0", "AV0", "AVP", "AVQ", "CJC", "CJS", "CJT", "CRD",
+        "DPS", "DT0", "DTQ", "EX0", "ITJ", "NN0", "NN1", "NN2", "NP0", "ORD", "PNI",
+        "PNP", "PNQ", "PNX", "POS", "PRF", "PRP", "PUL", "PUN", "PUQ", "PUR", "TO0",
+        "UNC", 'VBB', 'VBD', 'VBG', 'VBI', 'VBN', 'VBZ', 'VDB', 'VDD', 'VDG', 'VDI',
+        'VDN', 'VDZ', 'VHB', 'VHD', 'VHG', 'VHI', 'VHN', 'VHZ', 'VM0', 'VVB', 'VVD',
+        'VVG', 'VVI', 'VVN', 'VVZ', 'XX0', 'ZZ0', 'AJ0-AV0', 'AJ0-VVN', 'AJ0-VVD',
+        'AJ0-NN1', 'AJ0-VVG', 'AVP-PRP', 'AVQ-CJS', 'CJS-PRP', 'CJT-DT0', 'CRD-PNI', 'NN1-NP0', 'NN1-VVB',
+        'NN1-VVG', 'NN2-VVZ', 'VVD-VVN', 'AV0-AJ0', 'VVN-AJ0', 'VVD-AJ0', 'NN1-AJ0', 'VVG-AJ0', 'PRP-AVP',
+        'CJS-AVQ', 'PRP-CJS', 'DT0-CJT', 'PNI-CRD', 'NP0-NN1', 'VVB-NN1', 'VVG-NN1', 'VVZ-NN2', 'VVN-VVD']
+
+def read_from_file(filename):
+    """
+
+    """
+    f = open(filename)
+    lines = f.readlines()
+    counter = 0
+    words = []
+    for l in lines:
+        if counter < 20: # db
+            words.append(str(l))
+            counter += 1
+    words = np.array(words)
+    f.close()
+    return words
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--trainingfiles",
+        action="append",
+        nargs="+",
+        required=True,
+        help="The training files."
+    )
+    parser.add_argument(
+        "--testfile",
+        type=str,
+        required=True,
+        help="One test file."
+    )
+    parser.add_argument(
+        "--outputfile",
+        type=str,
+        required=True,
+        help="The output file."
+    )
+    args = parser.parse_args()
+
+    training_list = args.trainingfiles[0]
+    print("training files are {}".format(training_list))
+    # read the training files 
+    for file in training_list:
+        print("reading {}".format(file))
+        words = read_from_file(file)
+
+    # print first 20 lines of training file
+    for i in range(len(words)): 
+        print(words[i])
+    
+
+    
+
+    print("test file is {}".format(args.testfile))
+
+    print("output file is {}".format(args.outputfile))
+
+
+    print("Starting the tagging process.")

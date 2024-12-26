c31c6df5eddeedc7fc2be562c5fb0abe86ec24bb
NOBODIDI <adrienmeryusa@gmail.com>
2024-12-26 15:01:24 -0500

    Hua Rong Dao Presentation

--- HuaRongDao_dfs_astar/hrd.py
+++ HuaRongDao_dfs_astar/hrd.py
@@ -14,8 +14,7 @@ from state import State
 
 start = time.time() 
 
-
-def read_from_file(filename):
+def read_from_file(filename: str) -> Board:
     """
     Load initial board from a given file.
     :param filename: The name of the given file.
@@ -23,7 +22,6 @@ def read_from_file(filename):
     :return: A loaded board
     :rtype: Board
     """
-
     puzzle_file = open(filename, "r")
 
     line_index = 0
@@ -31,44 +29,50 @@ def read_from_file(filename):
     g_found = False
 
     for line in puzzle_file:
-
         for x, ch in enumerate(line):
-
-            if ch == const.CHAR_VER_UP: # found vertical piece
+            if ch == const.CHAR_VER_UP:  # found vertical piece
                 pieces.append(Piece(False, False, x, line_index, 'v'))
-            elif ch == const.CHAR_HOR_LEFT: # found horizontal piece
+            elif ch == const.CHAR_HOR_LEFT:  # found horizontal piece
                 pieces.append(Piece(False, False, x, line_index, 'h'))
             elif ch == const.CHAR_SINGLE:
                 pieces.append(Piece(False, True, x, line_index, None))
             elif ch == const.CHAR_GOAL:
-                if g_found == False:
+                if not g_found:
                     pieces.append(Piece(True, False, x, line_index, None))
                     g_found = True
         line_index += 1
     puzzle_file.close()
     board = Board(pieces)
-    
     return board
 
-def man_dist(state): 
+def man_dist(state: State) -> int:
     """
     Calculate the Manhattan distance of the current state.
-    Man distance is the distance of the 2*2 piece to the exit
+    Man distance is the distance of the 2x2 piece to the exit.
     :param state: The current state.
     :type state: State
     :return: The Manhattan distance of the current state.
     :rtype: int
     """
-
     dist = 1
     for piece in state.board.pieces:
         if piece.is_goal:
-            distance = abs(3 - piece.coord_y) + abs(1 - piece.coord_x)
+            dist = abs(3 - piece.coord_y) + abs(1 - piece.coord_x)
     return dist
 
-def add_succesor(state, successors, i, x_coord, y_coord):
+def add_successor(state: State, successors: list, i: int, x_coord: int, y_coord: int):
     """
     Add a new successor to the successor list.
+    :param state: The current state.
+    :type state: State
+    :param successors: The list of successors.
+    :type successors: list
+    :param i: The index of the piece to move.
+    :type i: int
+    :param x_coord: The new x coordinate of the piece.
+    :type x_coord: int
+    :param y_coord: The new y coordinate of the piece.
+    :type y_coord: int
     """
     # creating a deep copy of the board pieces
     new_pieces = copy.deepcopy(state.board.pieces)
@@ -88,8 +92,7 @@ def add_succesor(state, successors, i, x_coord, y_coord):
         new_state.f = man_dist(new_state) + new_state.depth
     successors.append(new_state)
 
-
-def gen_successors(state): 
+def gen_successors(state: State) -> list:
     """
     Generate the successors of the current state.
     :param state: The current state.
@@ -97,114 +100,99 @@ def gen_successors(state):
     :return: The successors of the current state.
     :rtype: list[State]
     """
-
     successors = []
     i = 0
-    
     for piece in state.board.pieces:
-
         # single piece
         if piece.is_single:
             # space above
             if piece.move_valid(piece.coord_x, piece.coord_y - 1):
                 if state.board.grid[piece.coord_y - 1][piece.coord_x] == '.':
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y - 1)
-
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y - 1)
             # space below    
             if piece.move_valid(piece.coord_x, piece.coord_y + 1):
                 if state.board.grid[piece.coord_y + 1][piece.coord_x] == '.': 
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y + 1)
-                    
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y + 1)
             # space left    
             if piece.move_valid(piece.coord_x - 1, piece.coord_y):
                 if state.board.grid[piece.coord_y][piece.coord_x - 1] == '.': 
-                    add_succesor(state, successors, i, piece.coord_x - 1, piece.coord_y)
-                    
+                    add_successor(state, successors, i, piece.coord_x - 1, piece.coord_y)
             # space right
             if piece.move_valid(piece.coord_x + 1, piece.coord_y):
                 if state.board.grid[piece.coord_y][piece.coord_x + 1] == '.':
-                    add_succesor(state, successors, i, piece.coord_x + 1, piece.coord_y)
-                    
-        
+                    add_successor(state, successors, i, piece.coord_x + 1, piece.coord_y)
         # vertical piece
         if piece.orientation == 'v': 
             # space above
             if piece.move_valid(piece.coord_x, piece.coord_y - 1):
                 if state.board.grid[piece.coord_y - 1][piece.coord_x] == '.':
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y - 1)
-
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y - 1)
             # space below    
             if piece.move_valid(piece.coord_x, piece.coord_y + 1):
                 if state.board.grid[piece.coord_y + 2][piece.coord_x] == '.': 
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y + 1)
-                    
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y + 1)
             # space left    
             if piece.move_valid(piece.coord_x - 1, piece.coord_y):
                 if (state.board.grid[piece.coord_y][piece.coord_x - 1] == '.'
                 and state.board.grid[piece.coord_y + 1][piece.coord_x - 1] == '.'): 
-                    add_succesor(state, successors, i, piece.coord_x - 1, piece.coord_y)
-                    
+                    add_successor(state, successors, i, piece.coord_x - 1, piece.coord_y)
             # space right
             if piece.move_valid(piece.coord_x + 1, piece.coord_y):
                 if (state.board.grid[piece.coord_y][piece.coord_x + 1] == '.'
                 and state.board.grid[piece.coord_y + 1][piece.coord_x + 1] == '.'):
-                    add_succesor(state, successors, i, piece.coord_x + 1, piece.coord_y)
-
+                    add_successor(state, successors, i, piece.coord_x + 1, piece.coord_y)
         # horizontal piece
         if piece.orientation == 'h':
             # space above
             if piece.move_valid(piece.coord_x, piece.coord_y - 1):
                 if (state.board.grid[piece.coord_y - 1][piece.coord_x] == '.'
                 and state.board.grid[piece.coord_y - 1][piece.coord_x + 1] == '.'):
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y - 1)
-
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y - 1)
             # space below    
             if piece.move_valid(piece.coord_x, piece.coord_y + 1):
                 if (state.board.grid[piece.coord_y + 1][piece.coord_x] == '.'
                 and state.board.grid[piece.coord_y + 1][piece.coord_x + 1] == '.'):
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y + 1)
-                    
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y + 1)
             # space left 
             if piece.move_valid(piece.coord_x - 1, piece.coord_y):
                 if state.board.grid[piece.coord_y][piece.coord_x - 1] == '.': 
-                    add_succesor(state, successors, i, piece.coord_x - 1, piece.coord_y)
-                    
+                    add_successor(state, successors, i, piece.coord_x - 1, piece.coord_y)
             # space right
             if piece.move_valid(piece.coord_x + 1, piece.coord_y):
                 if state.board.grid[piece.coord_y][piece.coord_x + 2] == '.':
-                    add_succesor(state, successors, i, piece.coord_x + 1, piece.coord_y)
-        
+                    add_successor(state, successors, i, piece.coord_x + 1, piece.coord_y)
         # goal piece 
         if piece.is_goal: 
             # space above
             if piece.move_valid(piece.coord_x, piece.coord_y - 1):
                 if (state.board.grid[piece.coord_y - 1][piece.coord_x] == '.'
                 and state.board.grid[piece.coord_y - 1][piece.coord_x + 1] == '.'):
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y - 1)
-
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y - 1)
             # space below    
             if piece.move_valid(piece.coord_x, piece.coord_y + 1):
                 if (state.board.grid[piece.coord_y + 2][piece.coord_x] == '.'
                 and state.board.grid[piece.coord_y + 2][piece.coord_x + 1] == '.'): 
-                    add_succesor(state, successors, i, piece.coord_x, piece.coord_y + 1)
-                    
+                    add_successor(state, successors, i, piece.coord_x, piece.coord_y + 1)
             # space left    
             if piece.move_valid(piece.coord_x - 1, piece.coord_y):
                 if (state.board.grid[piece.coord_y][piece.coord_x - 1] == '.'
                 and state.board.grid[piece.coord_y + 1][piece.coord_x - 1] == '.'): 
-                    add_succesor(state, successors, i, piece.coord_x - 1, piece.coord_y)
-                    
+                    add_successor(state, successors, i, piece.coord_x - 1, piece.coord_y)
             # space right
             if piece.move_valid(piece.coord_x + 1, piece.coord_y):
                 if (state.board.grid[piece.coord_y][piece.coord_x + 2] == '.'
                 and state.board.grid[piece.coord_y + 1][piece.coord_x + 2] == '.'):
-                    add_succesor(state, successors, i, piece.coord_x + 1, piece.coord_y)
+                    add_successor(state, successors, i, piece.coord_x + 1, piece.coord_y)
         i += 1
     return successors
 
-def get_solution(state):
+def get_solution(state: State) -> list:
     """
-    Returns a list of states that lead to a solution
+    Returns a list of states that lead to a solution.
+    :param state: The final state.
+    :type state: State
+    :return: The list of states leading to the solution.
+    :rtype: list[State]
     """
     solution = []
     while state.parent:
@@ -213,18 +201,22 @@ def get_solution(state):
     solution.append(state)
     return solution[::-1]
 
-def dfs(state):
+def dfs(state: State) -> State:
     """
-    Depth-first search algorithm 
+    Depth-first search algorithm.
+    :param state: The initial state.
+    :type state: State
+    :return: The final state if a solution is found, None otherwise.
+    :rtype: State
     """
-    frontier = [state] # keep this a list
-    explored = set() # make this a set
+    frontier = [state]  # keep this a list
+    explored = set()  # make this a set
     # is frontier empty? 
     while frontier:
         # select and remove state Curr from frontier
         state = frontier.pop()
         # is Curr in explored? 
-        if (state.id not in explored):
+        if state.id not in explored:
             # add Curr to explored
             explored.add(state.id)
             # is Curr a goal state? 
@@ -237,18 +229,22 @@ def dfs(state):
                 frontier.append(successor)
     return None
 
-def astar(state):
+def astar(state: State) -> State:
     """
-    A* search algorithm
+    A* search algorithm.
+    :param state: The initial state.
+    :type state: State
+    :return: The final state if a solution is found, None otherwise.
+    :rtype: State
     """
-    frontier = [state] # keep this a list
-    explored = set() # make this a set
+    frontier = [state]  # keep this a list
+    explored = set()  # make this a set
     # is frontier empty? 
     while frontier:
         # select and remove state Curr from frontier
         state = frontier.pop(0)
         # is Curr in explored? 
-        if (state.id not in explored):
+        if state.id not in explored:
             # add Curr to explored
             explored.add(state.id)
             # is Curr a goal state? 
@@ -264,7 +260,6 @@ def astar(state):
     return None
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--inputfile",
@@ -304,13 +299,9 @@ if __name__ == "__main__":
 
     # output solution to file
     output_file = open(args.outputfile, "w")
-    #db 
-    # i = 0
     for state in solution:
-        # output_file.write(str(i) + '\n')
         state.board.display_file(output_file)
         output_file.write('\n')
-        # i += 1
     output_file.close()
 
     end = time.time()
